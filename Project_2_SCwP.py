56e63912feff27cd2b19206fcd0415c2ca03f9e7
harismallick <mallick.haris@gmail.com>
2022-07-24 04:11:12 +0100

    progess on project 2

--- Project_2_SCwP.py
+++ Project_2_SCwP.py
@@ -17,18 +17,66 @@ of the result. The day of the week in the output should appear after the time an
 
 def add_time(time, duration, day=None):
 
+    dur_in_mins = duration_convert(duration)
+    pm = False
 
-    return
+    # Convert time to 24-hour format, then into minutes for addition
+
+    t_split = time.split(':')
+    hour = int(t_split[0])
+    mins = int(t_split[1][:-3])
+    #print(mins)
+    if 'PM' in time:
+        hour = hour + 12
+
+    start_time = (hour * 60) + mins
+    new_time = start_time + dur_in_mins
+    print(new_time)
+
+    new_hour = new_time // 60
+    new_mins = new_time % 60
+    if new_hour > 24:
+        days_count = new_hour // 24
+        new_hour = new_hour % 24
+        
+    if new_hour > 12:
+        pm = True
+        new_hour = new_hour - 12
+
+    if pm:
+        time_print = f'{str(new_hour)}:{str(new_mins)} PM'
+
+    else:
+        time_print = f'{str(new_hour)}:{str(new_mins)} AM'
+
+    print(time_print)
+
+    
 
 def duration_convert(duration):
 
     hour_mins = duration.split(':')
-    print(hour_mins)
+    print(f'Duration variables: {hour_mins}')
+
+    hours = int(hour_mins[0])
+    mins = int(hour_mins[1])
+    time_to_add = (hours * 60) + mins # All times to be added in minutes
+    print(f'Time for add in mins: {time_to_add}')
+
+    return time_to_add
+    
+
+
     
 
 def main():
-    add_time('3:00 PM', '3:10')
-    duration_convert('3:10')
+    add_time('6:30 PM', '205:12')
+    #duration_convert('3:10')
 
 if __name__ == '__main__':
     main()
+
+# test = 370
+# floor_div = test//60
+# rem = test % 60
+# print(floor_div, rem)
